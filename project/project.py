1fd518c79e62ffc6e3e390abf62b60b83f30e362
zahra zamanoghli <zahra.zamanoghli@gmail.com>
2024-05-23 12:44:03 +0200

    updated project work 3

--- project/project.py
+++ project/project.py
@@ -2,6 +2,7 @@ import os
 import requests
 import pandas as pd
 import sqlite3
+from urllib.parse import urlparse
 
 data_urls = {
     "datasource1": "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/cei_gsr010?format=SDMX-CSV&compressed=false",
@@ -10,29 +11,39 @@ data_urls = {
 
 data_dir = "../data"
 
-def download(url, filename):
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+CYAN = '\033[36m'
+RESET = '\033[0m'
+
+def get_filename_from_url(url):
+    return os.path.basename(urlparse(url).path)
+
+def download(url, ds_key):
     response = requests.get(url)
-    file_path = os.path.join(data_dir, filename)
+    filename = get_filename_from_url(url)
+    file_path = os.path.join(data_dir, f"{ds_key}_{filename}.csv")
     with open(file_path, 'wb') as file:
         file.write(response.content)
+    print(GREEN + f"Downloaded {url} to {file_path}" + RESET)
     return file_path
 
 def transform(file_path):
     df = pd.read_csv(file_path)
     df.drop(["DATAFLOW", "OBS_FLAG"], axis=1, inplace=True)
+    print(YELLOW + "Data transformed" + RESET)
     return df
 
 def save(df, db_name, table_name):
     conn = sqlite3.connect(os.path.join(data_dir, db_name))
     df.to_sql(table_name, conn, if_exists='replace', index=False)
+    print(CYAN + f"Data saved to database {db_name} with table name {table_name}" + RESET)
     conn.close()
 
 def main():
     for name, url in data_urls.items():
-        file_path = download(url, f"{name}.csv")
-
+        file_path = download(url, name)
         df = transform(file_path)
-
         save(df, "dataset.db", name)
 
 if __name__ == "__main__":
